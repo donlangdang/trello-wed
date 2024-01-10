41dfc2587ec9f8273ab37ea377a578181af3cf5f
donlangdang <donlangdangcc@gmail.com>
2024-01-11 00:04:43 +0700

    set dark mode

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,4 @@
+import { cyan, deepOrange, orange, teal } from '@mui/material/colors'
 import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 
 
@@ -5,16 +6,14 @@ const theme = extendTheme({
   colorSchemes: {
     light: {
       palette: {
-        primary: {
-          main: '#ff5252'
-        }
+        primary: teal,
+        secondary: deepOrange
       }
     },
     dark: {
       palette: {
-        primary: {
-          main: '#000'
-        }
+        primary: cyan,
+        secondary: orange
       }
     }
   }
