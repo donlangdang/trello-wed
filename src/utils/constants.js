aceacb94cb799a50d0fbbc95f42b27fff2d7e207
donlangdang <donlangdangcc@gmail.com>
2024-02-19 21:30:47 +0700

    comment lai export const APT_ROOT = 'http://localhost:8017'

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const APT_ROOT = 'http://localhost:8017'
-// export const APT_ROOT = 'https://trello-wed-server.onrender.com'
+// export const APT_ROOT = 'http://localhost:8017'
+export const APT_ROOT = 'https://trello-wed-server.onrender.com'
