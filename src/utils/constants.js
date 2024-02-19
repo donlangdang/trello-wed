43e33ec638886f4b9ce4ad04e2a0a8b884f91b91
donlangdang <donlangdangcc@gmail.com>
2024-02-19 21:52:33 +0700

    Fix BUILD_MODE dev and production

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,12 @@
-// export const APT_ROOT = 'http://localhost:8017'
-export const APT_ROOT = 'https://trello-wed-server.onrender.com'
+let apiRoot = ''
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8017'
+}
+
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://trello-wed-server.onrender.com'
+}
+export const APT_ROOT = apiRoot
+
+
+// import.meta.env học thêm cái này
